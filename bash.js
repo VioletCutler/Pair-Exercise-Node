d58ab2dfbb79467d8a309fd1ca112cf82c861569
Sara Sauceda <sarafsauceda@gmail.com>
2022-01-14 12:48:37 -0800

    first commit

---
+++ bash.js
@@ -0,0 +1,9 @@
+const pwd = require('./pwd');
+console.log(Object.keys(pwd))
+
+pwd();
+
+const ls = require('./ls');
+console.log(Object.keys(ls))
+
+ls();
---
+++ pwd.js
@@ -0,0 +1,14 @@
+module.exports = function (){
+ process.stdout.write('prompt > ');
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    if(cmd === 'pwd'){
+    process.stdout.write(process.cwd())
+    }
+    //process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ' );
+})
+}
+
+// module.exports.prompt = prompt
+// module.exports.pwd = pwd
